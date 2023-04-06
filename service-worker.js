a6160f378f81489ad2bd444a17e210f2ecd24631
pmj0815 <90832550+pmj0815@users.noreply.github.com>
2023-04-06 14:44:18 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'asdf'; // 캐쉬 이름을 설정합니다.
+
+const FILES_TO_CACHE = [
+    '/', // 캐쉬할 페이지 or 파일 들을 설정합니다.
+];
+
+self.addEventListener('install', (event) => {event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)))});
